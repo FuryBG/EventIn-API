c0d2489f27a69b9b4cfb2eb0d027c8eccbca2208
rstoyanov <rstoyanov@cleancodefactory.de>
2023-12-16 23:43:20 +0200

    added font, javascript, cookie consent partial, and responsive navigation

---
+++ PollApi/wwwroot/scripts/site.js
@@ -0,0 +1,19 @@
+﻿// Nav section
+
+(function () {
+    let hamburger = document.querySelector(".hamburger");
+    let exitNav = document.querySelector(".exit-button");
+
+    hamburger.addEventListener("click", () => toggleMobileNav(true));
+    exitNav.addEventListener("click", () => toggleMobileNav(false));
+    function toggleMobileNav(show) {
+        let mobileNav = document.querySelector(".mobile-nav");
+        if (show) {
+            mobileNav.style.right = 0;
+
+        }
+        else {
+            mobileNav.style.right = "-100%";
+        }
+    }
+})()
